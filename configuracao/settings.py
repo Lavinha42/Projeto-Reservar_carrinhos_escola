eb0c0e149c3891b0cd8e0c9d906d477cf9efdaf6
Lauana dos Santos <147860296+Lavinha42@users.noreply.github.com>
2026-05-29 16:07:38 -0300

    fix: corrige configuração do banco de dados

--- configuracao/settings.py
+++ configuracao/settings.py
@@ -96,8 +96,9 @@ if render_host:
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
         conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
