8a281934409b56ac845594be34323997d6e15122
Antonny234 <antonnygabrieldacostasilva6@gmail.com>
2026-06-02 14:33:37 -0300

    Update urls.py

--- reservas/urls.py
+++ reservas/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='longa'),
-    path('criar/', views.CriarConta, name='index'  ),
+    path('criar/', views.CriarConta, name='index'),
     path('entrar/', views.Entrar, name='longa'),
     path('Logar/', views.mural, name='mural' ),
     path(
@@ -17,4 +17,5 @@ urlpatterns = [
     path('painel/', views.mural_principal, name='mural_consulta'),
     path('carregar-mural/', views.carregar_mural, name='carregar_mural'),
     path('excluir-reserva/<int:reserva_id>/', views.excluir_reserva, name='excluir_reserva'),
+    path('atualizar-quantidade/', views.atualizar_quantidade, name='atualizar_quantidade'),
 ]
